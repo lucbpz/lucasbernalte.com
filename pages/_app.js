c7d42914cc4c43d5e92fc1366e2a87a9ff378517
Lucas Bernalte <lucas.bernalte@gmail.com>
2021-12-02 19:42:03 +0100

    feat(): add next-plausible

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import { Global, css } from "@emotion/react";
 import { DefaultSeo } from "next-seo";
 import { ChakraProvider, useColorMode } from "@chakra-ui/react";
 import Head from "next/head";
+import PlausibleProvider from "next-plausible";
 
 import theme from "../styles/theme";
 import { prismLightTheme, prismDarkTheme } from "../styles/prism";
@@ -48,43 +49,35 @@ const App = ({ Component, pageProps }) => {
   return (
     <ChakraProvider theme={theme}>
       <MDXProvider components={MDXComponents}>
-        <GlobalStyle>
-          <Head>
-            <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
-            <meta
-              content="width=device-width, initial-scale=1"
-              name="viewport"
-            />
-            <meta content="#ffffff" name="theme-color" />
-            <meta content="#ffffff" name="msapplication-TileColor" />
-            <meta
-              content="/static/favicons/browserconfig.xml"
-              name="msapplication-config"
-            />
-            <meta
-              content="biHkMOychciu9cqUw-vineouEgViHqhNYTF8AFh67JA"
-              name="google-site-verification"
-            />
-            <script
-              async
-              defer
-              data-domain="lucasbernalte.com"
-              src="https://plausible.io/js/plausible.js"
-            ></script>
-            <link
-              rel="alternate"
-              type="application/rss+xml"
-              title="RSS"
-              href="/feed.xml"
-            />
-            <script>
-              window.plausible = window.plausible || function(){" "}
-              {(window.plausible.q = window.plausible.q || []).push(arguments)}
-            </script>
-          </Head>
-          <DefaultSeo {...SEO} />
-          <Component {...pageProps} />
-        </GlobalStyle>
+        <PlausibleProvider domain="lucasbernalte.com">
+          <GlobalStyle>
+            <Head>
+              <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
+              <meta
+                content="width=device-width, initial-scale=1"
+                name="viewport"
+              />
+              <meta content="#ffffff" name="theme-color" />
+              <meta content="#ffffff" name="msapplication-TileColor" />
+              <meta
+                content="/static/favicons/browserconfig.xml"
+                name="msapplication-config"
+              />
+              <meta
+                content="biHkMOychciu9cqUw-vineouEgViHqhNYTF8AFh67JA"
+                name="google-site-verification"
+              />
+              <link
+                rel="alternate"
+                type="application/rss+xml"
+                title="RSS"
+                href="/feed.xml"
+              />
+            </Head>
+            <DefaultSeo {...SEO} />
+            <Component {...pageProps} />
+          </GlobalStyle>
+        </PlausibleProvider>
       </MDXProvider>
     </ChakraProvider>
   );
