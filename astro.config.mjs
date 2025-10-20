1ff75054d6d6dff650c9758c9949da947cf13cd8
Lucas Bernalte <lucas.bernalte@gmail.com>
2025-10-20 11:28:11 -0700

    change astro to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,6 +21,6 @@ export default defineConfig({
       },
     }),
   ],
-  output: "server",
+  output: "static",
   adapter: cloudflare(),
 });
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -13,7 +13,10 @@ export function formatDate(date: Date) {
   }).format(date);
 }
 
-export function readingTime(html: string) {
+export function readingTime(html: string | undefined) {
+  if (!html) {
+    return "";
+  }
   const textOnly = html.replace(/<[^>]+>/g, "");
   const wordCount = textOnly.split(/\s+/).length;
   const readingTimeMinutes = (wordCount / 200 + 1).toFixed();
